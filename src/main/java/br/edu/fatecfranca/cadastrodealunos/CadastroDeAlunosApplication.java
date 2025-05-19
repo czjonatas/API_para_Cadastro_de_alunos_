9feb2c0ff16f5a46736be9ab448670d4cbe6881c
czjonatas <jonatascandido364@gmail.com>
2025-05-19 09:03:29 -0300

    meu primeiro programa web

---
+++ src/main/java/br/edu/fatecfranca/cadastrodealunos/CadastroDeAlunosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.cadastrodealunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeAlunosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeAlunosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/fatecfranca/cadastrodealunos/CadastroDeAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.cadastrodealunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeAlunosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
